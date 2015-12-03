9bd85d8c3a4cc049565fb4f2b89b6a0e4de92943
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2015-12-03 11:20:09 -0500

    Agregación de los íconos laterles para la función de noticias, ayuda y otros

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 /* Author: 
 
 */
+$( document ).ready(init);
 
+function init(){
+	cargarEventosPanel();
+}
+
+function cargarEventosPanel(){
+	$(".lateral-icon").click(function (e){
+		$(".panel-lateral").css("display","none");
+		var id = $(this).attr("data-open-id");
+		$("#"+id).css("display","block");
+	});
+}
 
 
 
